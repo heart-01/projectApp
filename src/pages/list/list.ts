f064510a710eac1bb5f18d3c373e9fcad24fe040
siwat jomewattana <s1104300051612@gmail.com>
2019-04-20 02:38:02 +0700

    getData

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the ListPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Toast } from '@ionic-native/toast';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { AlertController } from 'ionic-angular'; //popup message ธรมดา ต้องกด ok ถึงจะหาย
 
 @IonicPage()
 @Component({
@@ -15,11 +13,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ListPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  dataRoom:any[];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public toast: Toast,
+  public http: Http, private alertCtrl: AlertController) {
+
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ListPage');
+    this.getData();
   }
+
+  getData(){
+    this.http.get('http://localhost:8080/showRoom')
+    .map(res=>res.json())
+    .subscribe(data=>{
+      this.dataRoom=data;
+    });
+  }
+
+
+
 
 }
